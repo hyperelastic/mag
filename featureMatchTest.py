622baeb7fcc3f860ed6ca54665113879538e6820
hyperelastic <ador.elgrozni@gmail.com>
2014-01-21 11:22:05 +0100

    Izboljšava iskanja vogalov. Canny-jev algoritem.

--- featureMatchTest.py
+++ featureMatchTest.py
@@ -4,17 +4,18 @@ import cv2
 import numpy as np
 
 
+slika = cv2.imread('slikeKoda/niPoVrsti04.jpg', cv2.IMREAD_GRAYSCALE)
+# slika = cv2.GaussianBlur( slika, (5, 5), 0 );
+vogali = cv2.goodFeaturesToTrack(slika, 10, 0.01, 10)
+vogali = np.int0(vogali)
 
-im = cv2.imread('slikeKoda/niPoVrsti04.jpg', cv2.IMREAD_GRAYSCALE)
-imNP = np.float32(im)
-dst = cv2.cornerHarris(imNP, 3, 3, 0.04)
-dst = cv2.dilate(dst, None)
-im[dst>0.6*dst.max()] = 255
-
+for v in vogali:
+    x,y = v.ravel()
+    cv2.circle(slika,(x,y),3,255,-1)
 
 #imshow()
 cv2.namedWindow("Slikca1", cv2.WINDOW_AUTOSIZE)
 cv2.startWindowThread()
-cv2.imshow("Slikca1", im)
+cv2.imshow("Slikca1", slika)
 cv2.waitKey(3000)
 cv2.destroyWindow("Slikca1")
