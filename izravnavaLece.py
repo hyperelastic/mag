27373d89bcfd1c258a5331709e4425f423602eb6
hyperelastic <ador.elgrozni@gmail.com>
2014-01-28 22:17:11 +0100

    Dodan cas nastanka fotografije

--- izravnavaLece.py
+++ izravnavaLece.py
@@ -1,6 +1,16 @@
 import cv2
 import numpy as np
 from glob import glob
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def casZajema(imeDat):
+    ''' Vrne string s casom zajema hhmmss'''
+    slikaPil = Image.open(imeDat)
+    exif = {TAGS[k]: v for k, v in slikaPil._getexif().items() if k in TAGS}
+    return(exif['DateTimeOriginal'][-8:].translate(None, ':'))
+
+print(casZajema("slikeVzorec/000.jpg"))
 
 matr = np.loadtxt("paraParat/matrikaFotoaparata")
 popac = np.loadtxt("paraParat/popacenjeFotoaparata")
@@ -20,11 +30,12 @@ kartaX, kartaY = cv2.initUndistortRectifyMap(matr, popac, None,
 slike = glob('slikeVzorec/*.jpg')
 x,y,w,h = regZanim
 for sl in slike:
-    print("Izravnavam sliko " + sl[-7:])
+    imeIzravnane = sl[-7:-4] + "-" + casZajema(sl) + ".jpg"
+    print("Izravnavam sliko " + imeIzravnane)
     slikaOdpac = cv2.remap(cv2.imread(sl), kartaX, kartaY, cv2.INTER_LINEAR)
     slikaOdpac = slikaOdpac[y:y+h, x:x+w]
-    cv2.imwrite("slikeIzravnane/" + sl[-7:], slikaOdpac)
+    cv2.imwrite("slikeIzravnane/" + imeIzravnane, slikaOdpac)
 #     mala = cv2.resize(slikaOdpac, (0,0), fx=0.4, fy=0.4, interpolation=cv2.INTER_AREA)
 #     cv2.imshow('slikaOdpac', mala)
 #     cv2.waitKey(400)
-#     cv2.destroyAllWindows()
+# #     cv2.destroyAllWindows()
