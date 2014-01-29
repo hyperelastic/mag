535eafd35c924fcdd2fe3ada968d78600733200c
hyperelastic <ador.elgrozni@gmail.com>
2014-01-29 21:18:06 +0100

    Dokoncano iskanje fiz koordinat vseh kod

--- poravnavaPrveSlike.py
+++ poravnavaPrveSlike.py
@@ -28,39 +28,38 @@ def izboljsajPolozajKode(koda, okolica):
     ujemi = naj.match(op1,op2)
     ujemi = sorted(ujemi, key = lambda x:x.distance)
 
-    notXY = np.array([ t1[d.queryIdx].pt for d in ujemi[:5] ])
-    venXY = np.array([ t2[d.trainIdx].pt for d in ujemi[:5] ])
+    notXY = np.array([ t1[d.queryIdx].pt for d in ujemi[:6] ])
+    venXY = np.array([ t2[d.trainIdx].pt for d in ujemi[:6] ])
     premik = np.average(notXY-venXY, axis=0)
     stDevPremik = np.std(notXY-venXY, axis=0)
     
     delta = premik - (np.array(okolica[0].shape) - np.array(koda[0].shape))/2
     koda[2] = [x+d for x, d in zip(koda[2], delta)]
     
-#     print(premik)
-#     print(stDevPremik)
-#     print(delta)
-#     print("\n")
-#     srKodeNaKodi = [x/2 for x in koda[0].shape]
-#     srKodeNaOkolici = tuple([sr+d for sr, d in zip(srKodeNaKodi, delta)])
-#     cv2.circle(okolica[0], (int(srKodeNaOkolici[0]),
-#                                 int(srKodeNaOkolici[1])),
-#                                     3, 255, 2)
-# 
-#     sl3 = cv2.drawMatches(okolica[0], t1, koda[0], t2, ujemi[:5], 200, flags=2)
-#     cv2.imshow("Izboljsava", sl3)
-#     cv2.waitKey(500)
+    print("premik"); print(premik)
+    print("stDevPremik"); print(stDevPremik)
+    print("delta"); print(delta)
+    print("\n")
+    srKodeNaKodi = [x/2 for x in koda[0].shape]
+    srKodeNaOkolici = tuple([sr+d for sr, d in zip(srKodeNaKodi, premik)])
+    cv2.circle(okolica[0], (int(srKodeNaOkolici[0]),
+                                int(srKodeNaOkolici[1])),
+                                    8, 255, 1, cv2.LINE_AA)
+
+    sl3 = cv2.drawMatches(okolica[0], t1, koda[0], t2, ujemi[:6], 200, flags=2)
+    cv2.imshow("Izboljsava", sl3)
+    cv2.waitKey(1000)
     
     return(koda)
 
 
-def zapisiFizKoordinateKod(merilo, razdX, razdY, zamikX, zamikY):
-    Tocke = []
-    Tocke.append([0.0, 3*razdY])
-    for j in range(3, -1, -1):
-        Tocke.append([razdX, razdY*j])
-    for j in range(0, 3):
-        Tocke.append([0.0, razdY*j])
-    Tocke = np.array(Tocke)
+
+def fizKoordinateVogalov(merilo, zamikX, zamikY):
+    Tocke = np.array([  [0.0, 1500.0],
+                        [2361.0, 1500.0],
+                        [2365.0, 0.0],
+                        [0.0, 0.0]   ])
+
     Tocke[:,1] = Tocke[:,1]+zamikX
     Tocke[:,0] = Tocke[:,0]+zamikY
     return(np.float32(Tocke*merilo).reshape(-1,1,2))
@@ -70,14 +69,18 @@ def zapisiFizKoordinateKod(merilo, razdX, razdY, zamikX, zamikY):
 prva = cv2.imread(imePrve, cv2.IMREAD_GRAYSCALE)
 prvaMala = cv2.resize(prva, (0,0), fx=fakPomanj, fy=fakPomanj, interpolation=cv2.INTER_AREA)
 
+
+
 kode = []
 kodeMale = []
 imenaSlikKod = sorted(glob('slikeKoda/[0-20]*.jpg'))
-for iSK in imenaSlikKod:    #kode[i] = [koda, stKode, [polozX, polozY]]
+for iSK in imenaSlikKod:
     koda = cv2.imread(iSK, cv2.IMREAD_GRAYSCALE)
     kode.append([koda, int(iSK[-6:-4]), [0, 0]])
     koda = cv2.resize(koda, (0,0), fx=fakPomanj, fy=fakPomanj, interpolation=cv2.INTER_AREA)
     kodeMale.append([koda, int(iSK[-6:-4]), [0, 0]])
+
+
 
 for koda, kodaMala in zip(kode, kodeMale):
     res = cv2.matchTemplate(prvaMala, kodaMala[0], cv2.TM_SQDIFF)
@@ -90,44 +93,46 @@ for koda, kodaMala in zip(kode, kodeMale):
                                 cv2.FONT_HERSHEY_SIMPLEX, fakPomanj, 255)
 
 
-kode = kode[3:11]
-kodeMale = kodeMale[3:11]
+
+kodeRobovi = [kode[i] for i in [3,4,7,8]]
+kodeRoboviMale = [kodeMale[i] for i in [3,4,7,8]]
+
+
+
 okolice = []
-for koda in kode:
+for koda in kodeRobovi:
     xSr, ySr = koda[2][0], koda[2][1]
     okolice.append([prva[ySr-vOko:ySr+vOko, xSr-vOko:xSr+vOko], [xSr, ySr]])
 
-vhodneHom = []
-for okolica, koda, kodaMala in zip(okolice, kode, kodeMale):
+
+
+vhodniVogali = []
+for okolica, koda, kodaMala in zip(okolice, kodeRobovi, kodeRoboviMale):
     koda = izboljsajPolozajKode(koda, okolica)
-    vhodneHom.append(koda[2])
+    cv2.destroyAllWindows()
+    vhodniVogali.append(koda[2])
     kodaMala[2] = [int(k*fakPomanj) for k in koda[2]]
-#     cv2.circle(prvaMala, tuple(kodaMala[2]), 3, 255, 1, cv2.LINE_AA)
-#     print("Natancen polozaj kode " + str(kodaMala[1]) + " je:")
-#     print(koda[2])
-# cv2.imshow('slika', prvaMala)
-# cv2.waitKey(5000)
-# cv2.destroyAllWindows()
-
-vhodneHom = np.float32(vhodneHom).reshape(-1,1,2)
-izhodneHom = zapisiFizKoordinateKod(merilo, 2363.0, 500.0, 100.0, 100.0)
-
-print(vhodneHom.shape)
-print(izhodneHom.shape)
+vhodniVogali = np.float32(vhodniVogali).reshape(-1,1,2)
+izhodniVogali = fizKoordinateVogalov(merilo, 120.0, 120.0)
 
 
-M, void = cv2.findHomography(vhodneHom, izhodneHom, cv2.LMEDS, 5.0)
-# M = cv2.getPerspectiveTransform()
 
-print(M)
-# 
-prvaMalaNova = cv2.warpPerspective(prvaMala, M, tuple(prvaMala.shape))
+M = cv2.getPerspectiveTransform(vhodniVogali, izhodniVogali)
 
-cv2.imshow('slika', prvaMalaNova)
-cv2.waitKey(5000)
-cv2.destroyAllWindows()
 
-cv2.imwrite('prvaMalaNova.jpg', prvaMalaNova)
+
+vhodneKoordinateVseh = []
+for koda in kode:
+    vhodneKoordinateVseh.append(koda[2])
+vhodneKoordinateVseh = np.float32(vhodneKoordinateVseh).reshape(-1,1,2)
+fizKoordinateVseh = cv2.perspectiveTransform(vhodneKoordinateVseh, M)
+print fizKoordinateVseh[:,0,:]
+np.savetxt("paraParat/lokacijeKod", fizKoordinateVseh[:,0,:])
+np.savetxt("paraParat/faktorPomanjsanjaSlike", [fakPomanj])
+
+
+# prvaNova = cv2.warpPerspective(prva, M, (int(3698+120*merilo), int(2400+120*merilo)))
+# cv2.imwrite('prvaNova.jpg', prvaNova)
 
 
 
