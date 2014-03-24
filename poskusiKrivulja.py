e861ce00109b7d0eb2aea82f2ec8ea930b3b21d9
KlemenC <ador.elgrozni@gmail.com>
2014-03-24 19:37:51 +0100

    Kar fajn delovanje, testirano, malo se spuct.

--- poskusiKrivulja.py
+++ poskusiKrivulja.py
@@ -7,8 +7,8 @@ cv2.namedWindow("Prikaz", cv2.WINDOW_NORMAL)
 imena = sorted(glob('./slikePoravnane/nelin/*.jpg'))
 
     
-
-for ime in imena[434:443]:
+kot = 0
+for ime in imena[155:156]:
     print(ime)
     sl = cv2.imread(ime)
     clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(6,6))
@@ -24,36 +24,62 @@ for ime in imena[434:443]:
     zacToc = [izKan1[1]-izKan1[0]-97, 100]
     cv2.circle(slKan1, (zacToc[1], zacToc[0]), 1, 255, thickness=1)
 
-    cv2.imshow("Prikaz", slKan1[1300:,:-1900])
-    cv2.waitKey(2000)
+    #cv2.imshow("Prikaz", slKan1[1300:,:-1900])
+    #cv2.waitKey(2000)
     
+    kot = 0
     radij = 10
-    for j in range(1):
+    for j in range(194):
+        print(j)
         slOkence = slKan1[zacToc[0]-2*radij:zacToc[0]+2*radij,
                              zacToc[1]-2*radij:zacToc[1]+2*radij]
         slOkence = np.fliplr(slOkence)
         slOkence = cv2.linearPolar(slOkence, (2*radij, 2*radij), int(1.1*radij),
                                     cv2.INTER_LINEAR)
-        #cv2.circle(slOkence, (2*radij, 2*radij), radij, 255, thickness=1, 
-        #                lineType = cv2.LINE_AA)
 
+        slPas = slOkence[1.5*radij:-0.5*radij,-0.2*radij-1:]
+        slTemplate = 180*np.uint8(np.ones((9,np.shape(slPas)[1])))
+        slTemplate[3:6,:] = np.uint8(40+np.zeros((3,np.shape(slPas)[1])))
 
-        slPas = slOkence[1.0*radij:-1.0*radij,-0.2*radij-1:]
-        slTemplate = 180*np.uint8(np.ones((7,np.shape(slPas)[1])))
-        slTemplate[2:5,:] = np.uint8(20+np.zeros((3,np.shape(slPas)[1])))
+        #cv2.imshow("Prikaz", slOkence)
+        #cv2.waitKey(1000)
+        #cv2.imshow("Prikaz", slPas)
+        #cv2.waitKey(10000)
+        #cv2.imshow("Prikaz", slTemplate)
+        #cv2.waitKey(10000)
 
-        cv2.imshow("Prikaz", slOkence)
-        cv2.waitKey(10000)
-        
-        slPas = cv2.matchTemplate(slPas, slTemplate, cv2.TM_SQDIFF_NORMED)
-        #print(slPas)
-        #print(slPasKvadrat)
-        #matPoloz = np.uint16(np.transpose(np.transpose(np.ones(np.shape(slPas)))\
-        #                            *np.array(range(np.shape(slPas)[0]))))
-        #matPoloz *= slPasKvadrat
-        #povprecje = (1.0*np.sum(matPoloz))/np.sum(slPasKvadrat)
-        cv2.imshow("Prikaz", slPas)
-        cv2.waitKey(10000)
+        slPas = cv2.matchTemplate(slPas, slTemplate, cv2.TM_CCORR_NORMED)
+        slPas = cv2.resize(slPas, (0, 0), fx = 1, fy=5)
+        slPas = cv2.GaussianBlur(slPas, (11, 11),sigmaX=5, sigmaY=10)
+        #cv2.imshow("Prikaz", slPas)
+        #cv2.waitKey(1000)
+        void, void, void, maxLoc = cv2.minMaxLoc(slPas); 
+        maxLoc = float(maxLoc[1])/5
+
+        #povprecje za natancnejsi ujem
+        #slPas = slPas[maxLoc-2:maxLoc+3, 0]
+        #matPoloz = np.array((1,2,3,4,5))
+        #matPoloz *= slPas
+        #maxLoc += np.sum(matPoloz)/np.sum(slPas) - 3.
+        maxLoc += 1.5*radij+4.5     #zamik zaradi matchTemplate v velikosti slTemplate
+        print(maxLoc)
+
+        #kot = 0.5*(kot + 0.5*np.pi*(float(maxLoc-2*radij)/radij))
+        #kot =  0.5*np.pi*(float(maxLoc-2*radij)/radij)
+        kot = 0.5*(kot + 0.5*np.pi*(float(maxLoc-2*radij)/radij))
+        zacToc = [zacToc[0]-radij*np.sin(kot), zacToc[1]+radij*np.cos(kot)]
+        print("kot: " + str(kot) + "\n")
+
+        #cv2.imshow("Prikaz", slPas)
+        #cv2.waitKey(10000)
+
+        cv2.circle(slOkence, (3*radij, int(maxLoc)), 1, 255, thickness=1)
+        cv2.circle(slKan1, tuple([int(zacToc[1]), 
+                            int(zacToc[0])]), 2, 215, thickness=2)
+        #cv2.imshow("Prikaz", slOkence)
+        #cv2.waitKey(5)
+    cv2.imshow("Prikaz", slKan1[800:,-300:-100])
+    cv2.waitKey(10000)
 
 #kanal1 = cv2.extractChannel(sl, 1)
 #visIzreza = 800; sirIzreza = 1500
